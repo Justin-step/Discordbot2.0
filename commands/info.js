585ecf638c556e5df7185b5117be6f93338f0be4
Justin-step <100309424+Justin-step@users.noreply.github.com>
2022-04-10 18:20:24 +0200

    .
    .

--- commands/info.js
+++ commands/info.js
@@ -7,10 +7,10 @@ module.exports.run = async (client, message, args) => {
     .setDescription("Ik ben altijdche en stream vaak op twitch wat ik stream: Rust, Fivem, Phasmophobia en nog veel meer!")
     .setColor("#ff0000")
     .addField("Bot naam", client.user.username)
-    .setThumbnail('https://cdn.discordapp.com/attachments/689802923156439060/943981948806791178/IMG_5048.png')
-    .setImage('https://cdn.discordapp.com/attachments/689802923156439060/943981948806791178/IMG_5048.png')
+    .setThumbnail('foto')
+    .setImage('foto')
     .setTimestamp()
-    .setFooter("OnzDorp-RP", 'https://cdn.discordapp.com/attachments/689802923156439060/943981948806791178/IMG_5048.png');
+    .setFooter("AltijdChe", 'Foto');
 
     return message.channel.send({ embeds: [botEmbed] });
 
